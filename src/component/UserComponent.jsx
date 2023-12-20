1700514daf5f267683f2dc193321d2ed1db2c64b
tip <cirzxs@gmail.com>
2023-12-20 14:52:07 +0700

    add follower and following count

--- src/component/UserComponent.jsx
+++ src/component/UserComponent.jsx
@@ -17,6 +17,8 @@ export default function UserComponent() {
     const [isFollowed, setIsFollowed] = useState(false);
     const [followText, setFollowText] = useState("Follow");
     const [isYourUser, setIsYourUser] = useState(false);
+    const [totalFollowers, setTotalFollowers] = useState(0);
+    const [totalFollowing, setTotalFollowing] = useState(0);
 
     function handleFollow() {
         setLoadingFollow(true);
@@ -25,8 +27,10 @@ export default function UserComponent() {
             const isFollowedResult = response.data.data.isFollowed;
             setIsFollowed(isFollowedResult);
             if (isFollowedResult) {
+                setTotalFollowers(totalFollowers + 1);
                 setFollowText("Unfollow")
             } else {
+                setTotalFollowers(totalFollowers - 1);
                 setFollowText("Follow")
             }
             setLoadingFollow(false);
@@ -40,12 +44,14 @@ export default function UserComponent() {
     useEffect(() => {
         CommonService.getUserByUsername(usernameParam)
         .then(response => {
-            setUserID(response.data.data.user.id);
-            setUsername(response.data.data.user.username);
-            setProfileImage(response.data.data.user.profileImage);
-            setDisplayName(response.data.data.user.displayName);
+            setUserID(response.data.data.id);
+            setUsername(response.data.data.username);
+            setProfileImage(response.data.data.profileImage);
+            setDisplayName(response.data.data.displayName);
             setIsFollowed(response.data.data.isFollowed);
             setIsYourUser(response.data.data.isYourUser);
+            setTotalFollowers(response.data.data.totalFollowers);
+            setTotalFollowing(response.data.data.totalFollowing);
             if (response.data.data.isFollowed) {
                 setFollowText("Unfollow");
             }
@@ -66,6 +72,15 @@ export default function UserComponent() {
                 <Avatar alt={username} src={profileImage} sx={{ position: 'relative',width: 256, height: 256, "&:hover": { cursor: 'pointer' } }}></Avatar>
                 <Typography sx={{ mt: 4,fontWeight: 'bold', fontSize: 'large' }}>{displayName || username}</Typography>
                 <Typography color="text.secondary" sx={{ fontSize: 'default' }}>@{username}</Typography>
+                <Box display='flex' flexDirection='row' justifyContent='space-evenly' alignItems='center' sx={{width: 200, height:70}}>
+                    <Box display='flex' flexDirection='column' alignItems='center' sx={{width:100}}>
+                        <Typography color="text.primary" sx={{ fontSize: 'default' }}>Following</Typography><Typography color="text.primary" sx={{ fontSize: 'bold' }}>{totalFollowing}</Typography>
+                    </Box>
+                    <Box display='flex' flexDirection='column' alignItems='center' sx={{width:100}}>
+                        <Typography color="text.primary" sx={{ fontSize: 'default' }}>Followers</Typography><Typography color="text.primary" sx={{ fontSize: 'bold' }}>{totalFollowers}</Typography>
+                    </Box>
+                    
+                </Box>
                 <LoadingButton disabled={isYourUser} color={isFollowed ? "error" : "primary"} loading={loadingFollow} onClick={handleFollow} variant="contained" sx={{ mt: 2 }}>{followText}</LoadingButton>
             </Box>
             
