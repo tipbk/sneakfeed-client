d5a86fca14c13291e33bded7d7cfecdfd5960e82
tip <cirzxs@gmail.com>
2023-12-21 12:04:50 +0700

    cleanup unused stuff

--- src/page/LoginPage.jsx
+++ src/page/LoginPage.jsx
@@ -1,28 +1,11 @@
-import React, { useState } from 'react';
-import AuthService from '../services/authService'
-import ConfigService from '../services/configService';
+import React from 'react';
 import LoginComponent from '../component/LoginComponent';
 
 function LoginPage() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        AuthService.login(username, password)
-        .then(response => {
-            ConfigService.setAccessToken(response.data.data.accessToken);
-            ConfigService.setRefreshToken(response.data.data.refreshToken);
-            window.location.href = `/`;
-        })
-        .catch(err => {
-            alert("incorrect username or password")
-        })
-    }
 
     return (
         <React.Fragment>
-            <LoginComponent headerText="Login" username={username} setUsername={setUsername} password={password} setPassword={setPassword} handleSubmit={handleSubmit} buttonText="Login"/>
+            <LoginComponent />
         </React.Fragment>
     );
 }
